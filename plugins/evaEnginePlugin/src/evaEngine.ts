076a1c9742111f1477ce4eab496ee1c1919a1cf4
liho <liho@revenuemonster.my>
2025-01-23 13:33:49 +0800

    fix: update EVA client URL and prefix to use EVAL

--- plugins/evaEnginePlugin/src/evaEngine.ts
+++ plugins/evaEnginePlugin/src/evaEngine.ts
@@ -2,8 +2,8 @@ import { CHROMIA_CHAIN, EvaClient } from "@superoo7/eva-sdk";
 
 export const initEvaClient = async (privateKey: string) => {
   const evaClient = await EvaClient.init(privateKey, CHROMIA_CHAIN.MAINNET, {
-    url: "https://api.evaengine.ai/api/v1",
-    prefix: "EVA",
+    url: "https://api.evalengine.ai/api/v1",
+    prefix: "EVAL",
     pub: "026822066B64608E0A6E071D8AE76BDE509011FF823DBBF9FD6AC2E1F202904A0A",
   });
 
