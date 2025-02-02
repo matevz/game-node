8364153e606eab4053eae2b669597fb526ac8a9b
Ocenka <testoviydiman1@gmail.com>
2025-02-02 16:37:39 +0000

    Fix spelling errors in `example.ts` (#25)
    * fix spelling example.ts
    
    * fix spelling example.ts

--- plugins/rss3Plugin/src/example.ts
+++ plugins/rss3Plugin/src/example.ts
@@ -12,8 +12,8 @@ const rss3Plugin = new RSS3Plugin({
 
 const agent = new GameAgent(process.env.GAME_API_KEY ?? "", {
   name: "RSS3 Worker",
-  goal: "Retrive account activities via the RSS3 Network.",
-  description: `You are an AI agent with the ability to access real-time activities via the RSS3 Network. You take in a blockchain wallet address (begining with 0x) or an ENS domain (ending with .eth) and retrieve the activities associated with the account.
+  goal: "Retrieve account activities via the RSS3 Network.",
+  description: `You are an AI agent with the ability to access real-time activities via the RSS3 Network. You take in a blockchain wallet address (beginning with 0x) or an ENS domain (ending with .eth) and retrieve the activities associated with the account.
   `,
   workers: [
     rss3Plugin.getWorker({
