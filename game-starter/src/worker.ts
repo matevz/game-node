8a8a782e3b3d74c4d27f11eb9e4afe146bf5e2aa
John Soh <johnsoh@Johns-MacBook-Pro.local>
2025-02-11 12:47:37 -0600

    replacing game-starter example with activity-recommender example, and removing activity-recommender from examples folder; and also adding a short readme on how to run examples

--- game-starter/src/worker.ts
+++ game-starter/src/worker.ts
@@ -1,30 +1,14 @@
 import { GameWorker } from "@virtuals-protocol/game";
-import { helloFunction, searchTweetsFunction, replyToTweetFunction, postTweetFunction } from "./functions";
+import { getWeatherFunction, getLocationFunction, recommendActivitiesFunction } from "./functions";
 
-export const helloWorker = new GameWorker({
-    id: "hello_worker",
-    name: "hello worker",
-    description: "has the ability to say hello",
-    functions: [helloFunction],
-    getEnvironment: async () => {
-        return {
-            status: 'friendly',
-            // Add any environment variables your worker needs
-            someLimit: 10,
-        };
-    },
-});
-
-export const postTweetWorker = new GameWorker({
-    id: "twitter_main_worker",
-    name: "Twitter main worker",
-    description: "Worker that posts tweets",
-    functions: [searchTweetsFunction, replyToTweetFunction, postTweetFunction],
-    // Optional: Provide environment to LLP
-    getEnvironment: async () => {
-        return {
-            tweet_limit: 15,
-        };
-    },
-});
-
+// Create a demo worker with our functions
+export const activityRecommenderWorker = new GameWorker({
+    id: "activity_recommender",
+    name: "Activity Recommender",
+    description: "Gets location and weather information and recommends activities",
+    functions: [
+        getLocationFunction,
+        getWeatherFunction,
+        recommendActivitiesFunction
+    ]
+}); 
