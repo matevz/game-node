587b6d9eada52d1840bd7d19d3c4f9b577f886e2
John Soh <johnsoh@Johns-MacBook-Pro.local>
2025-02-18 14:25:45 -0600

    adding LLM change in game-starter code and update package.json file

--- game-starter/src/agent.ts
+++ game-starter/src/agent.ts
@@ -1,4 +1,4 @@
-import { GameAgent } from "@virtuals-protocol/game";
+import { GameAgent, LLMModel } from "@virtuals-protocol/game";
 import { activityRecommenderWorker } from "./worker";
 import dotenv from "dotenv";
 dotenv.config();
@@ -12,6 +12,7 @@ export const activity_agent = new GameAgent(process.env.API_KEY, {
     goal: "Help users find the perfect activities based on their location and current weather conditions",
     description: "You are an agent that gets location of the user and then uses that to get weather information and then uses that to recommend activities",
     workers: [activityRecommenderWorker],
+    llmModel: LLMModel.DeepSeek_R1 // this is an optional paramenter to set the llm model for the agent. Default is Llama_3_1_405B_Instruct
 });
 
 activity_agent.setLogger((agent: GameAgent, msg: string) => {
