d3cb5e21f97d2ee390a0db817a230690992016e7
John Soh <johnsoh@Johns-MacBook-Pro.local>
2025-03-05 17:47:05 -0600

    adding better descriptions to functions

--- examples/state-management/functions.ts
+++ examples/state-management/functions.ts
@@ -3,7 +3,7 @@ import { restaurantInventory, restaurantBudget, ingredientPrices, updateRestaura
 import { getMoves, setAgentState } from "./agent"
 export const getIngredients = new GameFunction({
     name: "get_ingredients",
-    description: "Get the ingredients available in the kitchen",
+    description: "Returns a list of all ingredients currently available in the kitchen's inventory, showing the quantity of each ingredient",
     args: [
         {
             name: "ingredients",
@@ -23,7 +23,7 @@ export const getIngredients = new GameFunction({
 
 export const makeFood = new GameFunction({
     name: "make_food",
-    description: "Make the food",
+    description: "Creates a dish using the available ingredients in the kitchen inventory. This will return a suggestion for what can be prepared based on current stock",
     args: [
         {
             name: "ingredients",
@@ -43,7 +43,7 @@ export const makeFood = new GameFunction({
 
 export const buyIngredient = new GameFunction({
     name: "buy_ingredient",
-    description: "Buy the ingredient",
+    description: "Purchase a specific ingredient for the kitchen if there is sufficient budget. This will deduct the cost from the budget and add the ingredient to inventory",
     args: [
         { name: "currentBudget", description: "The current budget of the kitchen" },
         { name: "ingredient", description: "The ingredient to buy" },
@@ -70,7 +70,7 @@ export const buyIngredient = new GameFunction({
 
 export const getBudget = new GameFunction({
     name: "get_budget",
-    description: "Get the budget",
+    description: "Retrieves the current available budget for the kitchen, which can be used for purchasing ingredients",
     args: [] as const,
     executable: async (args, logger) => {
         return new ExecutableGameFunctionResponse(
@@ -82,7 +82,7 @@ export const getBudget = new GameFunction({
 
 export const getIngredientPrices = new GameFunction({
     name: "get_ingredient_prices",
-    description: "Get the prices of the ingredients",
+    description: "Returns a list of all available ingredients and their corresponding prices based on the ingredientPrices object. Use this information to plan purchases within budget",
     args: [] as const,
     hint: "use this to get the prices of the ingredients",
     executable: async (args, logger) => {
@@ -95,7 +95,7 @@ export const getIngredientPrices = new GameFunction({
 
 export const getMovesLeft = new GameFunction({
     name: "get_moves_left",
-    description: "Get the moves left",
+    description: "Shows the number of remaining moves or actions that can be taken in the current game session",
     args: [] as const,
     executable: async (args, logger) => {
         return new ExecutableGameFunctionResponse(
@@ -107,7 +107,7 @@ export const getMovesLeft = new GameFunction({
 
 export const doNothing = new GameFunction({
     name: "do_nothing",
-    description: "Do nothing",
+    description: "Skips the current turn without performing any action. This can be useful when waiting for specific conditions or saving moves",
     args: [] as const,
     executable: async (args, logger) => {
         return new ExecutableGameFunctionResponse(
